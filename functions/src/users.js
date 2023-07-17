424556768b60a1341aacb8eba4f0aa7e71230e08
Daniel Lancheros <daniellancheros@Daniels-MacBook-Air.local>
2023-07-17 15:51:31 -0400

    WIP

---
+++ functions/src/users.js
@@ -0,0 +1,23 @@
+import { db } from './dbConnect.js'
+
+const coll = db.collection ('users')
+
+export async function signup (req, res) {
+  const { email, password } = req.body
+  // TODO: hash passwords
+  await coll.insertOne({ email: email.toLowerCase(), password }) 
+  // Note: not checking if email already exists or doing any validation
+  login(req, res)
+}
+
+export async function login (req, res) {
+  const { email, password } = req.body 
+  let user = await coll.findOne({ email: email.lowerCase(), password })
+  delete user.password // strip out password 
+  // TODO: Create token and send with user below.
+  res.send ({ user })
+
+}
+// TODO: getProfile
+
+// TODO: editProfile
